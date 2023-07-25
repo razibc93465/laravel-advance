fe792b69bf19bbb89ad5165e45b9a1bf74a66aa5
Razib Chakraborti <razibc93465@gmail.com>
2023-07-25 14:47:49 +0600

    done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $user = new User();
+        $user->name = 'Test Name';
+        $user->email = 'test@gmail.com';
+        $user->password = Hash::make('12345678');
+        $user->save();
+
+        dd($user);
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Notifications/UserNotify.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserNotify extends Notification
+{
+    use Queueable;
+    private $user;
+
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    public function via($notifiable)
+    {
+        // return ['mail'];
+        return ['database'];
+    }
+
+    // public function toMail($notifiable)
+    // {
+    //     return (new MailMessage)
+    //                 ->line('The introduction to the notification.')
+    //                 ->action('Notification Action', url('/'))
+    //                 ->line('Thank you for using our application!');
+    // }
+
+    public function toArray($notifiable)
+    {
+        return [
+            'user_id' => $this->user['id'],
+            'name' => $this->user['name'],
+            'email' => $this->user['email']
+        ];
+    }
+}
