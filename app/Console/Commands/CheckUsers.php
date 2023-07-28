b2d95caadceae7fe1227359b7ee794d0d01752fa
Razib Chakraborti <razibc93465@gmail.com>
2023-07-28 11:44:54 +0600

    argument pass through artisan command

--- app/Console/Commands/CheckUsers.php
+++ app/Console/Commands/CheckUsers.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Command;
 class CheckUsers extends Command
 {
 
-    protected $signature = 'check:users';
+    protected $signature = 'check:users {userId?}';
 
     protected $description = 'Get count of users';
 
@@ -19,6 +19,19 @@ class CheckUsers extends Command
 
     public function handle()
     {
-        echo User::count() . "\n";
+        $total = User::count();
+        $userId = $this->argument('userId');
+
+        if ($userId) {
+            $user = User::find($userId);
+            if ($user) {
+                $user->delete();
+                $this->info("User {$user->name} removed for (ID: {$user->id}).");
+            } else {
+                $this->error("User with ID {$userId} not found.");
+            }
+        } else {
+            $this->info("Total User {$total}");
+        }
     }
 }
